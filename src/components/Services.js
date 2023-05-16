f963bffa056a8976173c7726336f9efdabfdcd4c
Hamza Husic <hamzahusic76@gmail.com>
2023-05-16 14:41:27 +0100

    servuce section css fix

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,7 +6,7 @@ const Services = () => {
     return ( 
         <section className="containter pt-20 lg:pt-32 pb-8 px-5 md:px-0">
             <h2 className='primaryFont text-center text-[26px] font-medium pb-5 tracking-wide px-0 md:px-5'>{langdb.serviceTitle}</h2>
-            <p className='text-center text-[16px    ] pb-10 px-0 md:px-5 font-thin'>{langdb.serviceDesc}</p>
+            <p className='text-center text-[16px] pb-10 px-0 md:px-5 font-thin'>{langdb.serviceDesc}</p>
             <div className='flex gap-2 md:gap-5 flex-wrap justify-evenly pt-5'>
                 {langdb.serviceCards.map((card,index) => (
                     <div className='max-w-[350px]' key={index}>
